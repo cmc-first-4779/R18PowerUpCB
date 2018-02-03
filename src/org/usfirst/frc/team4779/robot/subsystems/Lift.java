85a7f855de382a2ec2ddce28e1c54250359df956
sangyonghong <sangyonghong@10.0.1.104>
2018-02-03 13:09:35 -0500

    Updated the lift file

--- src/org/usfirst/frc/team4779/robot/subsystems/Lift.java
+++ src/org/usfirst/frc/team4779/robot/subsystems/Lift.java
@@ -1,11 +1,37 @@
 package org.usfirst.frc.team4779.robot.subsystems;
 
+import org.usfirst.frc.team4779.robot.RobotMap;
+import org.usfirst.frc.team4779.robot.commands.ClimberOff;
+
+import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
 public class Lift extends Subsystem {
+	Spark liftMotor = new Spark(RobotMap.liftMotor);
+	
+	public Lift() {
+		 super();
+	 }
+	
+	public void initDefaultCommand() {
+    	setDefaultCommand(new LiftOff());    
+    }
+    
+    public void climberOn() {
+    	climbMotor.set(1);	
+    }
+    public void climberOff() {
+    	climbMotor.set(0);	
+    }
+    
+	public void log() {
+	}
+}
+
+
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
