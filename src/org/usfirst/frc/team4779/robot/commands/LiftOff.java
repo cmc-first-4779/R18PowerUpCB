d9604a6a893758566b90f0d2960e1aee866af6ec
RobosapiensF <RobosapiensF@10.0.1.41>
2018-02-03 13:36:05 -0500

    filled out lift off command

--- src/org/usfirst/frc/team4779/robot/commands/LiftOff.java
+++ src/org/usfirst/frc/team4779/robot/commands/LiftOff.java
@@ -17,10 +17,12 @@ public class LiftOff extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.lift.liftOff();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.lift.liftOff();
     }
 
     // Make this return true when this Command no longer needs to run execute()
