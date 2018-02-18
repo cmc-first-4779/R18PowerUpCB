f19e710883884f5dfde2948e28a051ab9480d44f
Matt Hoxsie <matt.hoxsie@gmail.com>
2018-02-18 13:58:59 -0500

    Calibrating distances.

--- src/org/usfirst/frc/team4779/robot/autoCommands/CalibrateDistance.java
+++ src/org/usfirst/frc/team4779/robot/autoCommands/CalibrateDistance.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class CalibrateDistance extends CommandGroup {
 
     public CalibrateDistance() {
+    	//Using this to calibrate distances.
     	//addParallel(new SetLiftSetPointPID(RobotMap.LIFT_SETPOINT_HIGH_SPEED));
 		addSequential(new DriveStraightPID((277 - RobotMap.SCALE_THROTTLE_DOWN_DISTANCE), RobotMap.FRONT_SCALE_FULL_SPEED, RobotMap.FORWARD));
 		addSequential(new DriveStraightPID(RobotMap.SCALE_THROTTLE_DOWN_DISTANCE, RobotMap.THROTTLE_SPEED, RobotMap.FORWARD));
