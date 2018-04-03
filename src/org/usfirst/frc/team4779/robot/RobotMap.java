4c81a143ad0286bc550183141ca79b114c25740f
Cubert <Cubert@10.47.79.21>
2018-04-02 20:52:07 -0400

    Adjusted the distances so that we wouldn't come up short.  Also speeded up the switch speeds a bit.

--- src/org/usfirst/frc/team4779/robot/RobotMap.java
+++ src/org/usfirst/frc/team4779/robot/RobotMap.java
@@ -144,15 +144,15 @@ public class RobotMap {
 	public static double AISLE_SPEED = 0.8;
 	public static double AISLE_SCALE_APPROACH_DISTANCE = 37;
 	public static double FRONT_SWITCH_DISTANCE = 145;
-	public static double FRONT_SWITCH_SPEED = 0.6;
+	public static double FRONT_SWITCH_SPEED = 0.7;
 	public static double FRONT_SWITCH_APPROACH_DISTANCE = 13;
 	public static double AISLE_SWITCH_APPROACH_DISTANCE = 14;
-	public static double SIDE_SWITCH_DISTANCE = 94;
-	public static double SWITCH_AISLE_APPROACH_DISTANCE = 71;
+	public static double SIDE_SWITCH_DISTANCE = 98;
+	public static double SWITCH_AISLE_APPROACH_DISTANCE = 75;
 	public static double SWITCH_AISLE_DISTANCE = 24;
 	public static double LIFT_SETPOINT_HIGH_SPEED = 40;
 	public static double LIFT_SETPOINT_LOW_SPEED = 70;
-	public static double MIDDLE_SWITCH_DISTANCE = 90;
+	public static double MIDDLE_SWITCH_DISTANCE = 98;
 	public static double SCALE_ANGLE_DISTANCE = 255;
 	public static double FRONT_SCALE_ANGLE_APPROACH_DISTANCE = 42;
 }
